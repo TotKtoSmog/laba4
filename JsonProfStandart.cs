dfae1bec6ecdfe61ab6865a623b9e59b3dc0cbb3
Alexey Ustinov <TotKtoSmog513@gmail.com>
2023-12-13 23:51:00 +0300

    Получение манифеста JSON файла

---
+++ JsonProfStandart.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace laba4
+{
+    [DataContract]
+    public class JsonProfStandart
+    {
+        [DataMember]
+        public string authorFIO { get; set; }
+        [DataMember]
+        public string educationDirectionCode { get; set; }
+        [DataMember]
+        public string educationDirectionTitle { get; set; }
+        [DataMember]
+        public string fgosOrderNumber { get; set; }
+        [DataMember]
+        public string id { get; set; }
+        [DataMember]
+        public string educationLevelTitle { get; set; }
+        [DataMember]
+        public string title { get; set; }
+        [DataMember]
+        public string fgosOrderDate { get; set; }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,17 +12,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace laba4
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        public string PathJsonFile = @"C:\Users\Totkt\source\repos\laba4\data\file.json";
+        public JsonProfStandart jsonProfStandart { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            jsonProfStandart = LoadJsonFile(PathJsonFile);
+            this.DataContext = this;
+        }
+        public JsonProfStandart LoadJsonFile(string PathJsonFile)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(JsonProfStandart));
+            JsonProfStandart returnResult;
+            using (FileStream fs = new FileStream(PathJsonFile, FileMode.OpenOrCreate))
+            {
+                returnResult = (JsonProfStandart)serializer.ReadObject(fs);
+            }
+            return returnResult;
         }
     }
 }
